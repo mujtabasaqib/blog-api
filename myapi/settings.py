65e85e9a2296f6d5732bea29c4a0b9f7c6e4d8c2
mujtabasaqib <mujtabasaqib99@gmail.com>
2025-11-27 00:55:18 +0500

    updated settings.py

--- myapi/settings.py
+++ myapi/settings.py
@@ -27,8 +27,7 @@ SECRET_KEY = 'django-insecure-7$81!9zuqqj1-w9t5l@ww6w_6&tyo6om906@*8=%cyer+w$*(m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
@@ -122,6 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 # This production code might break development mode, so we check whether we're in DEBUG mode
 if not DEBUG:
